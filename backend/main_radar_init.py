79d2888c3e093d544e7fc57d50c5e91a0adf1852
ekurtgl <66868163+ekurtgl@users.noreply.github.com>
2022-11-15 19:43:35 -0600

    meeting 15 nov - iterate record - pred -  server

--- backend/main_radar_init.py
+++ backend/main_radar_init.py
@@ -11,29 +11,44 @@ radar_path = '/home/emre/Desktop/77ghz/open_radar/open_radar_initiative-new_rece
              'open_radar_initiative-new_receive_test/setup_radar/build'
 model_file = '/home/emre/PycharmProjects/RadarGUI/keras_model.h5'
 
-duration = 5  # sec
+duration = 1  # sec
 filename = 'raw_game'
 model_im_size = (224, 224)
 
 socketServer = IOServer()
 radar_mng = RadarManager(radar_path, cwd, sudo_password, storage_directory)
-# radar_mng.radar_init()
+radar_mng.radar_init()
 # radar_mng.record_and_plot(filename, duration=duration)
-pred = radar_mng.predict_sample(model_path=model_file, size=model_im_size)
+# pred = radar_mng.predict_sample(model_path=model_file, size=model_im_size)
+# pred = None
 
 
-def pred_to_server(eps):
+def pred_to_server(pred):
+    print('pred_to_server', pred)
     for i, p in enumerate(pred):
-        confidence = round(p * 100 + eps, 2)
+        confidence = round(p * 100 + 0, 2)
         socketServer.send('data', 'Class #' + str(i+1) + ' confidence: ' + str(confidence) + '%')
 
 
 def generate_continuous_pred():
+    # print('outer')
+    eps = 0
     while True:
-        eps = random.randint(0, 10)
-        print(eps)
-        pred_to_server(eps)
-        time.sleep(1)
+        try:
+            print('loop')
+            # socketServer.send('data', 'Class #')
+            radar_mng.record_and_plot(filename, duration=duration)
+            pred = radar_mng.predict_sample(model_path=model_file, size=model_im_size)
+            pred_to_server(pred)
+
+        except:
+            print('some error')
+    #     socketServer.send('data', 'Class #' + str(1) + ' confidence: ' + str(2) + '%')
+    # eps = random.randint(0, 10)
+    # eps = 0
+    # print(eps)
+    #     pred_to_server(eps)
+    # time.sleep(1)
 
 
 socketServer.on("initialize", lambda x: generate_continuous_pred())
