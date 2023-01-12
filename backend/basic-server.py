c170360e836271fd639ce5afb3053696aec1f96b
chris35469 <chris35469@gmail.com>
2023-01-11 19:16:38 -0600

    Update repo

---
+++ backend/basic-server.py
@@ -0,0 +1,19 @@
+from radar_utils.ioserver import IOServer
+
+socketServer = IOServer()
+
+def sendMessageToClient(message):
+    socketServer.send('data', message)
+
+def clientConnected():
+    print("Client connected")
+
+    ## Eventually we want to replace this call with a command from the radar signal processing pipeline
+    sendMessageToClient("I see you - from server.")
+
+
+socketServer.on("initialize", lambda x: clientConnected())
+
+socketServer.run()
+
+print(socketServer)
